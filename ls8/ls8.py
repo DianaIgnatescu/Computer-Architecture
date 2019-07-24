385fce83bf28597f3584006423899b7fa6e5fadb
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-07-24 13:53:41 +0100

    Check for second argument

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -5,6 +5,10 @@
 import sys
 from cpu import *
 
+if len(sys.argv) is not 2:
+    print("Missing second argument. Try typing python ls8.py example/mult.ls8")
+    sys.exit(1)
+
 cpu = CPU()
 
 cpu.load(sys.argv[1])
